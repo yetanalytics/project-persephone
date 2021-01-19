40c8fb5ffebede39cfdc6980961a9623642230b8
kelvinqian00 <kelvinqian2@gmail.com>
2021-01-19 13:03:01 -0500

    Update FSM tests to reflect refactor

--- test/com/yetanalytics/persephone_test/pattern_validation_test.clj
+++ test/com/yetanalytics/persephone_test/pattern_validation_test.clj
@@ -1,8 +1,6 @@
 (ns com.yetanalytics.persephone-test.pattern-validation-test
   (:require [clojure.test :refer :all]
-            [clojure.set :as cset]
             [clojure.zip :as zip]
-            #_[ubergraph.core :as uber]
             [com.yetanalytics.persephone.pattern-validation :as pv]
             [com.yetanalytics.persephone.utils.fsm :as fsm]))
 
@@ -205,77 +203,99 @@
                                  (pv/mapify-all ex-profile))))))
 
 (def template-1-fsm
-  (-> ex-profile :templates (get 0) pv/build-node-fsm))
+  (-> ex-profile :templates (get 0) pv/pattern->fsm fsm/nfa->dfa))
 
 (def pattern-1-fsm
-  (-> ex-profile :patterns (get 0)
+  (-> ex-profile
+      :patterns
+      (get 0)
       (pv/grow-pattern-tree (pv/mapify-all ex-profile))
-      pv/mechanize-pattern))
+      pv/pattern-tree->fsm))
 
 (def pattern-2-fsm
-  (-> ex-profile :patterns (get 2)
+  (-> ex-profile
+      :patterns
+      (get 2)
       (pv/grow-pattern-tree (pv/mapify-all ex-profile))
-      pv/mechanize-pattern))
+      pv/pattern-tree->fsm))
 
 (deftest build-node-fsm-test
   (testing "build-node-fsm function"
-    (is (= #{:type :symbols :states :start :accepts :transitions}
-           (set (keys template-1-fsm))))
-    #_(is (= (:accept template-1-fsm)
-           (fsm/read-next* template-1-fsm #{(:start template-1-fsm)}
-                           {:verb {:id "http://foo.org/verb1"}})))
-    #_(is (= #{}
-           (fsm/read-next* template-1-fsm #{(:start template-1-fsm)}
-                           {:verb {:id "http://foo.org/verb9"}})))))
+    (is (= {:next-states (-> template-1-fsm :accepts vec)
+            :rejected?   false
+            :accepted?   true}
+           (fsm/read-next template-1-fsm
+                          nil
+                          {:verb {:id "http://foo.org/verb1"}})))
+    (is (= {:next-states []
+            :rejected?   true
+            :accepted?   false}
+           (fsm/read-next template-1-fsm
+                          nil
+                          {:verb {:id "http://foo.org/verb9"}})))))
 
-(deftest mechanize-pattern-test
-  (testing "mechanize-pattern function"
-    (is (= #{:type :symbols :states :starts :accepts :transitions}
-           (set (keys pattern-1-fsm))))
-    (is (cset/superset? (:accept pattern-1-fsm)
-                        (fsm/read-next* pattern-1-fsm #{(:start pattern-1-fsm)}
-                                        {:verb {:id "http://foo.org/verb1"}})))
-    (is (cset/superset?
-         (:accept pattern-1-fsm)
-         (fsm/read-next* pattern-1-fsm
-                         (fsm/read-next* pattern-1-fsm #{(:start pattern-1-fsm)}
-                                         {:verb {:id "http://foo.org/verb2"}})
-                         {:verb {:id "http://foo.org/verb3"}})))
-    (is (= #{} (fsm/read-next* pattern-1-fsm #{(:start pattern-1-fsm)}
-                               {:verb {:id "http://foo.org/verb9"}})))
-    (is (= #{}
-           (fsm/read-next* pattern-1-fsm
-                           (fsm/read-next* pattern-1-fsm #{(:start pattern-1-fsm)}
-                                           {:verb {:id "http://foo.org/verb1"}})
-                           {:verb {:id "http://foo.org/verb1"}})))
-    (is (= #{:type :symbols :states :start :accepts :transitions}
-           (set (keys pattern-2-fsm))))
-    (is (cset/superset?
-         (:accept pattern-2-fsm)
-         (fsm/read-next* pattern-2-fsm #{(:start pattern-2-fsm)}
-                         {:verb {:id "http://foo.org/verb1"}})))
-    (is (cset/superset?
-         (:accept pattern-2-fsm)
-         (fsm/read-next* pattern-2-fsm
-                         (fsm/read-next* pattern-2-fsm #{(:start pattern-2-fsm)}
-                                         {:verb {:id "http://foo.org/verb1"}})
-                         {:verb {:id "http://foo.org/verb1"}})))
-    (is (= #{}
-           (fsm/read-next* pattern-2-fsm
-                           (fsm/read-next* pattern-2-fsm #{(:start pattern-2-fsm)}
-                                           {:verb {:id "http://foo.org/verb1"}})
-                           {:verb {:id "http://foo.org/verb9"}})))))
+(deftest mechanize-pattern-test-1
+  (testing "mechanize-pattern function on pattern #1"
+    (is (-> (fsm/read-next pattern-1-fsm
+                           nil
+                           {:verb {:id "http://foo.org/verb1"}})
+            :accepted?))
+    (is (let [s1 (-> (fsm/read-next pattern-1-fsm
+                                    nil
+                                    {:verb {:id "http://foo.org/verb2"}})
+                     :next-states
+                     first)]
+          (-> (fsm/read-next pattern-1-fsm
+                             s1
+                             {:verb {:id "http://foo.org/verb3"}})
+              :accepted?)))
+    (is (-> (fsm/read-next pattern-1-fsm
+                           nil
+                           {:verb {:id "http://foo.org/verb9"}})
+            :rejected?))
+    (is (let [s1 (-> (fsm/read-next pattern-1-fsm
+                                    nil
+                                    {:verb {:id "http://foo.org/verb1"}})
+                     :next-states
+                     first)]
+          (-> (fsm/read-next pattern-1-fsm
+                             s1
+                             {:verb {:id "http://foo.org/verb1"}})
+              :rejected?)))))
+
+(deftest mechanize-pattern-test-2
+  (testing "foo bar"
+    (is (-> (fsm/read-next pattern-2-fsm
+                           nil
+                           {:verb {:id "http://foo.org/verb1"}})
+            :accepted?))
+    (is (let [s1 (-> (fsm/read-next pattern-2-fsm
+                                    nil
+                                    {:verb {:id "http://foo.org/verb1"}})
+                     :next-states
+                     first)]
+          (-> (fsm/read-next pattern-2-fsm
+                             s1
+                             {:verb {:id "http://foo.org/verb1"}})
+              :accepted?)))
+    (is (let [s1 (-> (fsm/read-next pattern-2-fsm
+                                    nil
+                                    {:verb {:id "http://foo.org/verb1"}})
+                     :next-states
+                     first)]
+          (-> (fsm/read-next pattern-2-fsm
+                             s1
+                             {:verb {:id "http://foo.org/verb9"}})
+              :rejected?)))))
 
 (deftest profile-to-fsm-test
   (testing "profile-to-fsm function"
-    (is (= 2 (count (pv/profile-to-fsm ex-profile))))
-    (is (every? #(contains? % :start) (pv/profile-to-fsm ex-profile)))
-    (is (every? #(contains? % :accept) (pv/profile-to-fsm ex-profile)))
-    (is (every? #(contains? % :symbols) (pv/profile-to-fsm ex-profile)))
-    (is (every? #(contains? % :graph) (pv/profile-to-fsm ex-profile)))
-    #_(is (every? #(-> % :graph uber/ubergraph?) (pv/profile-to-fsm ex-profile)))
-    #_(is (not (:rejected-last
-              (fsm/read-next
-               (first (pv/profile-to-fsm ex-profile))
-               {:id "some-stmt-uuid"
-                :verb {:id "http://foo.org/verb1"}}))))))
+    (is (= 2 (count (pv/profile->fsm ex-profile))))
+    (is (every? #(= (-> % keys set)
+                    #{:type :symbols :states :start :accepts :transitions})
+                (pv/profile->fsm ex-profile)))
+    (is (not (-> (fsm/read-next (-> ex-profile pv/profile->fsm first)
+                                nil
+                                {:id "some-stmt-uuid"
+                                 :verb {:id "http://foo.org/verb1"}})
+                 :rejected?)))))

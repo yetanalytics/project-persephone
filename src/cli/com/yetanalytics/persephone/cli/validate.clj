996819403b036357377c3fdb7fb62737e742120d
kelvinqian00 <kelvinqian2@gmail.com>
2023-04-21 15:58:00 -0400

    Add all-valid and short-circuit args to validate

--- src/cli/com/yetanalytics/persephone/cli/validate.clj
+++ src/cli/com/yetanalytics/persephone/cli/validate.clj
@@ -13,7 +13,7 @@
     :parse-fn  f/read-profile
     :validate  [s/profile? s/profile-err-msg]
     :update-fn (fnil conj [])]
-   ["-i" "--template-id IRI" "IDs of Statement Templates to validate against; can specify one or more."
+   ["-i" "--template-id IRI" "IDs of Statement Templates to validate against; can specify zero or more."
     :id        :template-ids
     :multi     true
     :validate  [s/iri? s/iri-err-msg]
@@ -23,22 +23,37 @@
     :missing  "No Statement specified."
     :parse-fn f/read-statement
     :validate [s/statement? s/statement-err-msg]]
+   ["-a" "--all-valid"
+    (str "If set, the Statement is not considered valid unless it is valid against ALL Templates. "
+         "Otherwise, the Statement only needs to be valid against at least one Template.")
+    :id :all-valid]
+   ["-c" "--short-circuit"
+    (str "If set, then print on only the first Template the Statement fails validation against."
+         "Otherwise, print for all Templates the Statement fails against.")
+    :id :short-circuit]
    ["-h" "--help" "Display the help menu."]])
 
 (defn- validate
-  [{:keys [profiles template-ids statement]}]
+  [{:keys [profiles template-ids statement all-valid short-circuit]}]
   (let [compiled (per/compile-profiles->validators
                   profiles
                   :validate-profiles? false ; already validated as CLI args
                   :selected-templates (not-empty template-ids))]
-    (per/validate-statement compiled statement :fn-type :printer)))
+    (per/validate-statement compiled
+                            statement
+                            :fn-type :printer
+                            :all-valid? all-valid
+                            :short-circuit? short-circuit)))
 
 (defn -main [& args]
   (validate (a/handle-args args validate-statement-options)))
 
 (comment
+  (a/handle-args '("--help") validate-statement-options)
+
   (validate
    (a/handle-args
-    '("-p" "../poseidon/dev-resources/profile/tccc.jsonld" 
-      "-s" "sample-statement.json")
+    '("-p" "../poseidon/dev-resources/profile/calibration.jsonld" 
+      "-s" "sample-statement.json"
+      "-a" "-c")
     validate-statement-options)))

2a1b03e21439248d087bf473c34a302fd57cff58
kelvinqian00 <kelvinqian2@gmail.com>
2023-04-24 16:34:05 -0400

    Use persephone JSON utils when reading files

--- src/cli/com/yetanalytics/persephone/cli/util/file.clj
+++ src/cli/com/yetanalytics/persephone/cli/util/file.clj
@@ -1,25 +1,10 @@
 (ns com.yetanalytics.persephone.cli.util.file
-  (:require [clojure.java.io :as io]
-            [clojure.string  :as cstr]
-            [cheshire.core :as json]))
-
-(defn- coerce-profile-key [k]
-  (if (cstr/starts-with? k "@")
-    (keyword (cstr/replace-first k #"@" "_"))
-    (keyword k)))
+  (:require [com.yetanalytics.persephone.utils.json :as json]))
 
 (defn read-profile
   [profile-filename]
-  (with-open [reader (io/reader profile-filename)]
-    (let [res (json/parse-stream reader coerce-profile-key)]
-      res)))
-
-(defn read-template
-  [template-filename]
-  (with-open [reader (io/reader template-filename)]
-    (json/parse-stream reader coerce-profile-key)))
+  (json/coerce-profile (slurp profile-filename)))
 
 (defn read-statement
   [statement-filename]
-  (with-open [reader (io/reader statement-filename)]
-    (json/parse-stream reader)))
+  (json/coerce-statement (slurp statement-filename)))

fa6cdbff444ebdd9f2f2b881e3bf556e588f8750
kelvinqian00 <kelvinqian2@gmail.com>
2023-04-25 17:05:16 -0400

    Sketch out server namespace and alias

---
+++ src/server/com/yetanalytics/persephone/server.clj
@@ -0,0 +1,63 @@
+(ns com.yetanalytics.persephone.server
+  (:require [io.pedestal.http :as http]
+            [io.pedestal.interceptor :as i]
+            [com.yetanalytics.persephone :as per])
+  (:gen-class))
+
+(defn- health
+  []
+  (i/interceptor
+   {:name ::health
+    :enter (fn [_] {:status 200 :body "OK"})}))
+
+(def validate
+  (i/interceptor
+   {:name ::validate
+    :enter
+    (fn validate [context]
+      (let [{:keys [request]}     context
+            {:keys [json-params]} request
+            {:keys [profiles
+                    statement]}   json-params
+            ;; Compile and validate
+            compiled (per/compile-profiles->validators profiles)
+            err-res  (per/validate-statement compiled statement
+                                             :fn-type :errors)]
+        err-res))}))
+
+(def match
+  (i/interceptor
+   {:name ::match
+    :enter
+    (fn match [context]
+      (let [{:keys [request]}     context
+            {:keys [json-params]} request
+            {:keys [profiles
+                    statements]}  json-params
+            ;; Compile and match
+            compiled (per/compile-profiles->fsms profiles)
+            state-m (per/match-statement-batch compiled nil statements)]
+        state-m))}))
+
+(def routes
+  #{["/health"
+     :get [health]
+     :route-name :server/health]
+    ["/validate"
+     :post [validate]
+     :route-name :server/validate]
+    ["/match"
+     :post [match]
+     :route-name :server/match]})
+
+(defn- create-server []
+  (http/create-server
+   {::http/routes          routes
+    ::http/type            :jetty
+    ::http/allowed-origins []
+    ::http/host            "localhost"
+    ::http/port            8080
+    ::http/join?           false}))
+
+(defn -main [& _]
+  (http/start (create-server)))

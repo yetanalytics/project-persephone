f8a89fecfa19b64eabe625e2c20f3f011c8aa3cf
kelvinqian00 <kelvinqian2@gmail.com>
2023-10-11 15:45:37 -0400

    Add state-ref to persist FSM state

--- src/server/com/yetanalytics/persephone/server/match.clj
+++ src/server/com/yetanalytics/persephone/server/match.clj
@@ -23,27 +23,36 @@
     :multi     true
     :validate  [u/iri? u/iri-err-msg]
     :update-fn u/conj-argv]
+   ["-s" "--persist-state"
+    "If the current state of the pattern FSM should be retained."
+    :id :persist-state]
    ["-h" "--help" "Display the 'match' subcommand help menu."]])
 
-(defonce match-ref
-  (atom {:matchers nil}))
+(defonce state-ref
+  (atom {:matchers nil
+         :state    nil
+         :persist? false}))
 
 (defn compile-patterns!
   "Parse `arglist`, compile Profiles into FSMs, and store them in-memory.
-   Return either `:help`, `:error`, or `nil`."
+   Also set the initial state and state persistence mode.
+   Return either `:help`, `:error`, or `true`."
   [arglist]
   (let [parsed  (cli/parse-opts arglist match-statements-options)
         options (u/handle-parsed-args parsed)]
     (if (keyword? options)
       options
-      (try (let [{:keys [profiles pattern-ids compile-nfa]}
+      (try (let [{:keys [profiles pattern-ids compile-nfa persist-state]}
                  options
                  matchers
                  (per/compile-profiles->fsms
                   profiles
                   :compile-nfa? compile-nfa
                   :selected-patterns (not-empty pattern-ids))]
-             (swap! match-ref assoc :matchers matchers)
+             (swap! state-ref assoc
+                    :matchers matchers
+                    :state    nil
+                    :persist? (boolean persist-state))
              true)
            (catch ExceptionInfo e
              (u/print-assert-errors e)
@@ -53,5 +62,8 @@
   "Perform validation on `statements` against the FSM matchers that
    are stored in-memory in the webserver."
   [statements]
-  (let [{:keys [matchers]} @match-ref]
-    (per/match-statement-batch matchers nil statements)))
+  (let [{:keys [matchers persist? state]} @state-ref
+        state* (per/match-statement-batch matchers state statements)]
+    (when persist?
+      (swap! state-ref assoc :state state*))
+    state*))
